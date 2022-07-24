d037912d6eda0cdd4d75b72f993cf3be2d3fc077
tkiryu | Tatsushi Kiryu <tatsushi_kiryu@caddi.jp>
2022-07-24 17:26:59 +0900

    feat: use jsx

--- hello.tsx
+++ hello.tsx
@@ -1,7 +1,32 @@
-import { serve } from 'https://deno.land/std@0.140.0/http/server.ts';
+/** @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
 
-serve((_req) => {
-  return new Response('Hello World!', {
-    headers: { 'content-type': 'text/plain' },
+import { serve } from 'https://deno.land/std@0.140.0/http/server.ts';
+import { h, renderSSR } from 'https://deno.land/x/nano_jsx@v0.0.20/mod.ts';
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX</title>
+      </head>
+      <body>
+        <h1>Hello world!</h1>
+      </body>
+    </html>
+  );
+}
+
+function handler(req) {
+  const html = renderSSR(<App />);
+  return new Response(html, {
+    headers: {
+      'content-type': 'text/html',
+    },
   });
-});
+}
+
+serve(handler);
